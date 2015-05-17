e7a403af75ec2340b50f554c3daccacaf3ebd558
Allan Rojas <allanjose91@gmail.com>
2015-05-17 11:42:01 -0600

    Version 6.8.6

---
+++ sql/Procedures/insertComp_ArtList.sql
@@ -0,0 +1,31 @@
+-- Procedure para insertar en la lista de componentes de un articulo
+select * from FACTURAcOMPRA_OBJ;
+select * from articulos_obj;
+select * from componente_obj;
+
+CREATE OR REPLACE Procedure Insert_Art_Comp_List
+   ( art_id IN varchar2 , comp_id IN varchar2 )
+   
+IS
+ 
+BEGIN
+
+   insert into
+       table(
+             select list_ref_t_componente   from articulos_obj where codigo = art_id
+             )
+             values 
+             (
+             (SELECT REF(f) FROM Componente_OBJ f WHERE f.componente_id = comp_id)
+             );
+
+
+EXCEPTION
+WHEN OTHERS THEN
+   raise_application_error(-20001,'Error Insertando en lista Componentes- '||SQLCODE||' -ERROR- '||SQLERRM);
+END;
+
+execute Insert_Art_Comp_List ('QPKUSFSMOG','2');
+
+select list_ref_t_componente from articulos_obj where codigo = 'QPKUSFSMOG';
+  
