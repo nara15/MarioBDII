505e24e126c8b0ce8ecaa89676fcd6556d8967cb
Mario Naranjo <marionaranjo12@gmail.com>
2015-05-17 09:23:56 -0600

    Versión 6.7

--- sql/Creación de tipos/typeUsuarios.sql
+++ sql/Creación de tipos/typeUsuarios.sql
@@ -1,1 +1,41 @@
 --Creación del tipo T_Usuario
+CREATE OR REPLACE TYPE T_Usuario AS OBJECT
+  (
+    codigo     VARCHAR2 (20) ,
+    nombre     VARCHAR2 (50 CHAR) ,
+    nbrUsuario VARCHAR2 (15 CHAR) ,
+    pass       VARCHAR2 (20 CHAR) ,
+    CONSTRUCTOR FUNCTION T_Usuario(
+      pCodigo     IN VARCHAR2 ,
+      pNombre     IN VARCHAR2 ,
+      pNbrUsuario IN VARCHAR2 ,
+      pPass       IN VARCHAR2 ) RETURN SELF AS RESULT 
+  ) FINAL ;
+  /
+CREATE OR REPLACE TYPE BODY T_Usuario AS
+  CONSTRUCTOR FUNCTION T_Usuario(
+    pCodigo     IN VARCHAR2 ,
+    pNombre     IN VARCHAR2 ,
+    pNbrUsuario IN VARCHAR2 ,
+    pPass       IN VARCHAR2 ) RETURN SELF AS RESULT
+  AS
+  BEGIN
+    SELF.codigo := pCodigo;
+    SELF.nombre := pNombre;
+    SELF.nbrUsuario := pNbrUsuario;
+    SELF.pass := pPass;
+    RETURN;
+  END;
+END ;
+/
+
+--//////////////////////////////////////////////////////////////////////////////
+--Creación de la tabla de usuarios
+CREATE TABLE Usuarios_OBJ OF T_Usuario
+  (
+    codigo NOT NULL ,
+    nombre NOT NULL ,
+    nbrUsuario NOT NULL ,
+    pass NOT NULL ,
+    CONSTRAINT Usuario_OBJ_PK PRIMARY KEY ( codigo )
+  ) ;
--- sql/Creación de tipos/typesArticulos.sql
+++ sql/Creación de tipos/typesArticulos.sql
@@ -1,2 +1,62 @@
-
 --Creación del tipo T_Articulos
+create or replace TYPE T_Articulo AS OBJECT
+  (
+    codigo     VARCHAR2 (20 CHAR) ,
+    nombre     VARCHAR2 (50 CHAR) ,
+    marca      VARCHAR2 (20 CHAR) ,
+    modelo     VARCHAR2 (20) ,
+    cantMinima NUMBER (3) ,
+    cantMaxima NUMBER (5) ,
+    precio FLOAT ,
+    fechaActuPrecio DATE ,
+    fechaRegistro   DATE ,
+    usuarioInserta REF T_Usuario ,
+    ref_familia REF T_Familia ,
+    ref_T_UnidadMedida REF T_UnidadMedida ,
+    list_ref_T_Componente T_Componente_Lista ,
+    
+    CONSTRUCTOR FUNCTION T_Articulo(
+      pCodigo     IN VARCHAR2 ,
+      pNombre     IN VARCHAR2 ,
+      pMarca      IN VARCHAR2 DEFAULT 'PD' ,
+      pModelo     IN VARCHAR2 DEFAULT 'PD' ,
+      pCantMin    IN NUMBER ,
+      pCantMax    IN NUMBER ,
+      pPrecio     IN FLOAT ,
+      pCodFamilia IN VARCHAR2 )
+    RETURN SELF AS RESULT ,
+    MAP MEMBER FUNCTION precioMap RETURN FLOAT ) FINAL ;
+/
+create or replace TYPE BODY T_Articulo
+AS
+   MAP MEMBER FUNCTION precioMap RETURN FLOAT
+	AS
+	BEGIN
+  		 RETURN precio;
+	END;
+
+	CONSTRUCTOR FUNCTION T_Articulo(
+	    pCodigo     IN VARCHAR2 ,
+	    pNombre     IN VARCHAR2 ,
+	    pMarca      IN VARCHAR2 ,
+	    pModelo     IN VARCHAR2 ,
+	    pCantMin    IN NUMBER ,
+	    pCantMax    IN NUMBER ,
+	    pPrecio     IN FLOAT ,
+	    pCodFamilia IN VARCHAR2 )
+	  RETURN SELF AS RESULT AS
+		BEGIN
+			SELF.codigo := pCodigo;
+			SELF.nombre := pNombre;
+			SELF.marca := pMarca;
+			SELF.modelo := pModelo;
+			SELF.cantMinima := pCantMin;
+			SELF.cantMaxima := pCantMax;
+			SELF.precio := pPrecio;
+			SELF.ref_familia := null;
+		 	RETURN;
+		END;
+END ;
+/
+--//////////////////////////////////////////////////////////////////////////////
+--Creación de la tabla de usuarios
