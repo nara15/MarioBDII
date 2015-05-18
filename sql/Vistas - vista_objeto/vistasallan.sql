97af7d5b9fba7981e2344ed2cef6dd3365948210
aallanrd <allanjose91@gmail.com>
2015-05-18 05:48:14 -0500

    9.3

---
+++ sql/Vistas - vista_objeto/vistasallan.sql
@@ -0,0 +1,30 @@
+  -- Crear view de Clientes
+  CREATE OR REPLACE VIEW clientes_view OF t_clientes WITH OBJECT IDENTIFIER (codigo)
+  AS SELECT f.codigo, f.nombre, f.nombrecliente, (SELECT REF(f) FROM Info_OBJ f WHERE f.codigo = f.infocliente)
+  FROM clientes f;
+  
+  --Crear View de Usuarios
+  
+  
+  --Crear View Factura Compra
+  
+  CREATE OR REPLACE VIEW facturacompra_view OF t_facturacompra WITH OBJECT IDENTIFIER (codigo)
+  AS SELECT f.codigo, f.nombre, f.marca,f.modelo,to_number(f.cantmin),to_number(cantmax)
+               ,to_number(precio),NULL,
+  
+  (SELECT REF(f) FROM Proveedores_OBJ f WHERE f.codigo = f.proveedor),
+  (SELECT REF(f) FROM Usuarios_OBJ f WHERE f.codigo = f.usuario),
+  (SELECT REF(f) FROM unidadmedidad_OBJ f WHERE f.codigo = f.unidad)
+  FROM facturacompra f;
+  
+  
+  ALTER TABLE facturacompra
+  MODIFY fecharegistro varchar(50);
+  
+  select * from articulo;
+  select * from facturacompra_view;
+  
+  insert into facturacompra values ('F1','MiP','Marca','Model','1','20','5000','','1234','1234','PE');
+  insert into facturacompra values ('','MiP','Marca','Model','1','20','5000','','1234','1234','PE');
+
+ select * from unidadmedida
