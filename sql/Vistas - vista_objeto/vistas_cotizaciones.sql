01eea260c2ca31455790cf5b85e2d9696e1d0601
aallanrd <allanjose91@gmail.com>
2015-05-18 06:07:41 -0500

    9.4

--- sql/Vistas - vista_objeto/vistas_cotizaciones.sql
+++ sql/Vistas - vista_objeto/vistas_cotizaciones.sql
@@ -1,13 +1,44 @@
 --VISTAS////////////////////////////////////////////////////////////////////////
 
+
+CREATE TABLE Cotizaciones (
+         codigo      VARCHAR2(25) NOT NULL ,
+         fecha      VARCHAR2(25) NOT NULL ,
+         
+         condicionesPago      VARCHAR2(55) NOT NULL ,
+         condicionesEntrega      VARCHAR2(55) NOT NULL ,
+         vigencia      VARCHAR2(25) NOT NULL ,
+         observaciones      VARCHAR2(55) NOT NULL ,
+         listaPrecios      VARCHAR2(25) NOT NULL ,
+         moneda      VARCHAR2(25) NOT NULL ,
+         estado      VARCHAR2(25) NOT NULL ,
+         codigoCliente      VARCHAR2(25) NOT NULL CONSTRAINT cotizaciom_cliente_fk REFERENCES Clientes
+                     (codigo) ON delete Cascade,
+         codigoVendedor      VARCHAR2(25) NOT NULL CONSTRAINT cotizacion_usuario_fk REFERENCES Usuarios
+                     (cedula) ON delete Cascade 
+         
+        )
+         
+         
+   TABLESPACE proyecto
+   STORAGE ( INITIAL 50K);
+
+
 CREATE OR REPLACE VIEW cotizaciones_view OF t_cotizaciones WITH OBJECT IDENTIFIER (codigo)
-  AS SELECT f.codigo, f.fecha, f.estado
+  AS SELECT f.codigo, f.fecha, f.condicionespago, f.condicionesEntrega,
+  f.vigencia , f.observaciones , f.listaprecios, f.moneda , f.estado, (SELECT REF(f) FROM Clientes_OBJ f WHERE f.codigo = f.codigocliente),
+  (SELECT REF(f) FROM Usuarios_OBJ f WHERE f.codigovendedor = f.codigovendedor)
   FROM Cotizaciones f;
+  
 --------------------------------------------------------------------------------
 CREATE OR REPLACE VIEW componentes_view OF t_componente WITH OBJECT IDENTIFIER (componente_id)
 AS
   SELECT id_componente, null
   FROM Componentes;
+  
+  
+  
+  
 -------------------------------------------------------------------------------- 
 CREATE OR REPLACE VIEW articulos_view OF t_articulo WITH OBJECT IDENTIFIER (codigo)
 AS
